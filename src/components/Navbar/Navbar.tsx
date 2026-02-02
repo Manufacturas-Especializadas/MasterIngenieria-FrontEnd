49e5596417e7474884b510f269b57e73043060fe
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2026-02-02 11:20:54 -0600

    Sin padding en el eje de las X

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/logomesa.png";
 export const Navbar = () => {
   return (
     <header className="bg-primary border-b border-white/10 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-6">
+      <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between h-14">
           <Link to="/" className="flex items-center gap-3">
             <img src={Logo} alt="MESA" className="h-8 w-auto" />
