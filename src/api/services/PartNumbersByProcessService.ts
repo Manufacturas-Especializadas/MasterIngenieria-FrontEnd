47369c86f0df2e17ea5f3ccd83b9afa2246dc019
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-10 12:38:43 -0600

    Metodo para obtener las stats

--- src/api/services/PartNumbersByProcessService.ts
+++ src/api/services/PartNumbersByProcessService.ts
@@ -1,3 +1,14 @@
-class PartNumbersByProcessService {}
+import { API_CONFIG } from "../../config/api";
+import type { DashboardStats } from "../../types/Types";
+import { apiClient } from "../client";
+
+class PartNumbersByProcessService {
+  private getDashboardStatsEndpoint =
+    API_CONFIG.endpoints.partNumberByProcess.dashboardStats;
+
+  async getDashboardStats(): Promise<DashboardStats> {
+    return apiClient.get<DashboardStats>(this.getDashboardStatsEndpoint);
+  }
+}
 
 export const partNumbersByProcessService = new PartNumbersByProcessService();
