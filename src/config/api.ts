beb3e8791e59a22122b8c3f9d443731707bf0065
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-10 12:28:28 -0600

    Endpoint partNumberByProcess

--- src/config/api.ts
+++ src/config/api.ts
@@ -6,5 +6,9 @@ if (!API_BASE_URL) {
 
 export const API_CONFIG = {
   baseUrl: API_BASE_URL,
-  endpoints: {},
+  endpoints: {
+    partNumberByProcess: {
+      dashboardStats: "/api/PartNumbersByProcess/Dashboard-stats",
+    },
+  },
 };
