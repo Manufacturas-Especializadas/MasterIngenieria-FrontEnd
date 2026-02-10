353c47b336cb3344929414300ac2fbf3a8579781
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-10 15:25:36 -0600

    Cambio de nombre

--- src/api/services/PartNumbersByProcessService.ts
+++ src/api/services/PartNumbersByProcessService.ts
@@ -4,7 +4,7 @@ import { apiClient } from "../client";
 
 class PartNumbersByProcessService {
   private getParentPartNumbersDashboardStatsEndpoint =
-    API_CONFIG.endpoints.parentPartNumberByProcess.dashboardStats;
+    API_CONFIG.endpoints.partNumbersByProcess.dashboardStats;
 
   async getParentPartNumbersDashboardStats(): Promise<DashboardStats> {
     return apiClient.get<DashboardStats>(
