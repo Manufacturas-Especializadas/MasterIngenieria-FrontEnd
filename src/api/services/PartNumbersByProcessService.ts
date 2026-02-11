c03192aeb80cc76dc45bd03b36e1aae99115ad98
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-11 07:50:35 -0600

    Obtener numeros de parte hijos

--- src/api/services/PartNumbersByProcessService.ts
+++ src/api/services/PartNumbersByProcessService.ts
@@ -6,10 +6,17 @@ class PartNumbersByProcessService {
   private getParentPartNumbersDashboardStatsEndpoint =
     API_CONFIG.endpoints.partNumbersByProcess.getParentPartNumbers;
 
+  private getChildPartNumbersEndpoint =
+    API_CONFIG.endpoints.partNumbersByProcess.getChildPartNumbers;
+
   async getParentPartNumbersDashboardStats(): Promise<DashboardStats> {
     return apiClient.get<DashboardStats>(
       this.getParentPartNumbersDashboardStatsEndpoint,
     );
+  }
+
+  async getChildPartNumbers(): Promise<DashboardStats> {
+    return apiClient.get<DashboardStats>(this.getChildPartNumbersEndpoint);
   }
 }
 
