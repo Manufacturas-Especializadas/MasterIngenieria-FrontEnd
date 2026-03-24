37d13e6ebf1b48c199fe87afcdce1a99b76aab7f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-24 15:41:38 -0600

    fix: cambio de main

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { PartNumbersByProcessIndex } from "../pages/PartNumbersByProcess/PartNumbersByProcessIndex";
+import { CycleTimes } from "../pages/CycleTimes/CycleTimes";
 
 export const MyRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<PartNumbersByProcessIndex />} />
+      <Route path="/" element={<CycleTimes />} />
+      <Route
+        path="/numeros-de-parte-por-proceso"
+        element={<PartNumbersByProcessIndex />}
+      />
     </Routes>
   );
 };
